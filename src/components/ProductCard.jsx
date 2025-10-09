a87fe69786672e59259e17adc859a99db260797f
nnamdi onwukwe <nnamdionwukwe@gmail.com>
2025-10-09 14:09:55 +0100

    cd

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -12,7 +12,7 @@ const ProductCard = ({ product }) => {
       <div className="product-details">
         <h3 className="product-name">{product.name}</h3>
         <p className="product-price">{product.price}</p>
-        <p className="product-description">{product.description}</p>
+        {/* <p className="product-description">{product.description}</p> */}
         <button className="add-to-cart-btn">Add to Cart</button>
       </div>
     </div>
