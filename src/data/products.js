ac3706240428d6fc918eec08bbf587263cdaa13e
nnamdi onwukwe <nnamdionwukwe@gmail.com>
2025-10-11 22:23:35 +0100

    saw

--- src/data/products.js
+++ src/data/products.js
@@ -141,5 +141,38 @@ export const product = [
       "https://shoprite.ng/wp-content/uploads/2024/03/Chateau-Vertely-Pastoral-Wine-75Cl.jpg",
     description: "Tactile keys with customizable RGB lighting.",
   },
+
+  {
+    id: 1,
+    name: "Laptop X1 Carbon",
+    price: 1000,
+    imageUrl:
+      "https://shoprite.ng/wp-content/uploads/2024/03/Chateau-Vertely-Pastoral-Wine-75Cl.jpg",
+    description: "Ultra-lightweight and powerful business laptop.",
+  },
+  {
+    id: 2,
+    name: "Wireless Headphones",
+    price: 2000,
+    imageUrl:
+      "https://shoprite.ng/wp-content/uploads/2024/03/Chateau-Vertely-Pastoral-Wine-75Cl.jpg",
+    description: "Noise-cancelling with crystal-clear audio.",
+  },
+  {
+    id: 3,
+    name: "4K Monitor",
+    price: 3000,
+    imageUrl:
+      "https://shoprite.ng/wp-content/uploads/2024/03/Chateau-Vertely-Pastoral-Wine-75Cl.jpg",
+    description: "Vibrant colors and high resolution for professional use.",
+  },
+  {
+    id: 4,
+    name: "Mechanical Keyboard",
+    price: 4000,
+    imageUrl:
+      "https://shoprite.ng/wp-content/uploads/2024/03/Chateau-Vertely-Pastoral-Wine-75Cl.jpg",
+    description: "Tactile keys with customizable RGB lighting.",
+  },
   // Add more products as needed
 ];
