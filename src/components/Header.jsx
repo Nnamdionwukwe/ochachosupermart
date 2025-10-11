1120d75a3701c5e6634996fb1e654d0457441bba
nnamdi onwukwe <nnamdionwukwe@gmail.com>
2025-10-11 22:03:44 +0100

    de

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
           <i class="bi bi-cart-plus"></i>
         </Link>
 
-        <div className="user-profile">
+        <div className="user-profile1">
           <NavBar />
         </div>
       </div>
