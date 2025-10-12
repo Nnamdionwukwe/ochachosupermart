60cf32ed875daa122f69f536cb0215dd9b00cddf
nnamdi onwukwe <nnamdionwukwe@gmail.com>
2025-10-12 14:44:21 +0100

    xs

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -21,16 +21,19 @@ const ProductCard = ({ product }) => {
         />
       </Link>
       <div className={styles.productInfo}>
-        <h3 className={styles.ProductName}>{product.name}</h3>
+        <div className={styles.textContainer}>
+          <h3 className={styles.ProductName}>{product.name}</h3>
 
-        <div className={styles.priceContainer}>
-          <img
-            src={nairaIconUrl}
-            alt="Naira Symbol"
-            className={styles.nairaIcon}
-          />
-          <p className={styles.productPrice}>{product.price.toFixed(2)}</p>
+          <div className={styles.priceContainer}>
+            <img
+              src={nairaIconUrl}
+              alt="Naira Symbol"
+              className={styles.nairaIcon}
+            />
+            <p className={styles.productPrice}>{product.price.toFixed(2)}</p>
+          </div>
         </div>
+
         {/* <p className="product-description">{product.description}</p> */}
         {/* <i class="bi bi-cart-plus"></i> */}
         <div className={styles.buttonContainer}>
