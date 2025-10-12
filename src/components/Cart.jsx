731d30719f98d8e21f6bc114bf317bfe3c6d09de
nnamdi onwukwe <nnamdionwukwe@gmail.com>
2025-10-12 16:13:54 +0100

    de

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -14,37 +14,43 @@ const Cart = () => {
   );
 
   return (
-    <div className={styles.cartContainer}>
-      <h2>Shopping Cart</h2>
-      {cartItems.length === 0 ? (
-        <p className={styles.emptyMessage}>Your cart is empty.</p>
-      ) : (
-        <div className={styles.cartContent}>
-          <div className={styles.cartItemList}>
-            {cartItems.map((item) => (
-              <CartCard
-                key={item.id}
-                item={item}
-                removeFromCart={removeFromCart}
-                updateQuantity={updateQuantity}
-              />
-            ))}
-          </div>
-          <div className={styles.cartSummary}>
-            <h3>Order Summary</h3>
-            <div className={styles.cartTotal}>
-              <span>Total:</span>
-              <span className={styles.totalAmount}>#{total}</span>
+    <>
+      <Link to="/products">
+        <button className={styles.backBtn}>&larr;</button>
+      </Link>
+
+      <div className={styles.cartContainer}>
+        <h2>Shopping Cart</h2>
+        {cartItems.length === 0 ? (
+          <p className={styles.emptyMessage}>Your cart is empty.</p>
+        ) : (
+          <div className={styles.cartContent}>
+            <div className={styles.cartItemList}>
+              {cartItems.map((item) => (
+                <CartCard
+                  key={item.id}
+                  item={item}
+                  removeFromCart={removeFromCart}
+                  updateQuantity={updateQuantity}
+                />
+              ))}
             </div>
-            <Link to="/cart/checkout">
-              <button className={styles.checkoutBtn}>
-                Proceed to Checkout
-              </button>
-            </Link>
+            <div className={styles.cartSummary}>
+              <h3>Order Summary</h3>
+              <div className={styles.cartTotal}>
+                <span>Total:</span>
+                <span className={styles.totalAmount}>#{total}</span>
+              </div>
+              <Link to="/cart/checkout">
+                <button className={styles.checkoutBtn}>
+                  Proceed to Checkout
+                </button>
+              </Link>
+            </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 };
 
