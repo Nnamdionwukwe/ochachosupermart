b96288c2639be032297d632ce3c027b39a7c2c82
nnamdi onwukwe <nnamdionwukwe@gmail.com>
2025-10-12 23:19:13 +0100

    cd

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -1,14 +1,38 @@
 import CategoryNavLinkSlider from "./CategoryNavLinkSlider";
 import Header from "./Header";
 import ImageSlider from "./ImageSlider";
+import ProductList from "./ProductList";
+import Layout from "./Layout";
+
+import WhyChooseUs from "./WhyChooseUs";
+import supermarketImage from "../assets/dis3.jpeg"; // Path to your image
+
+const benefits = [
+  "Fresh, organic produce delivered daily from local farms. ",
+  "Competitive pricing with daily deals and discounts.",
+  "Fast and reliable delivery service right to your doorstep.",
+  "Wide selection of products to meet all your household needs.",
+  "Easy-to-use mobile app for a seamless shopping experience.",
+];
 
 function ProductsPage() {
   return (
     <div>
       <Header />
+
       <ImageSlider />
+
       <CategoryNavLinkSlider />
-      PRODUCTS PAGE
+
+      <ProductList />
+
+      <WhyChooseUs
+        title="Why Choose Us?"
+        image={supermarketImage}
+        benefits={benefits}
+      />
+
+      <Layout />
     </div>
   );
 }
