8a50e59e8fe33965fa8005f8662e9b4654497934
nnamdi onwukwe <nnamdionwukwe@gmail.com>
2025-10-12 22:38:27 +0100

    de

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,6 +4,17 @@ import ImageSlider from "./ImageSlider";
 import ProductList from "./ProductList";
 import Layout from "./Layout";
 
+import WhyChooseUs from "./WhyChooseUs";
+import supermarketImage from "../assets/dis3.jpeg"; // Path to your image
+
+const benefits = [
+  "Fresh, organic produce delivered daily from local farms.",
+  "Competitive pricing with daily deals and discounts.",
+  "Fast and reliable delivery service right to your doorstep.",
+  "Wide selection of products to meet all your household needs.",
+  "Easy-to-use mobile app for a seamless shopping experience.",
+];
+
 function HomePage() {
   return (
     <div>
@@ -14,6 +25,12 @@ function HomePage() {
       <CategoryNavLinkSlider />
 
       <ProductList />
+
+      <WhyChooseUs
+        title="Why Choose Us?"
+        image={supermarketImage}
+        benefits={benefits}
+      />
 
       <Layout />
     </div>
---
+++ src/components/WhyChooseUs.jsx
@@ -0,0 +1,36 @@
+import styles from "./WhyChooseUs.module.css";
+
+const WhyChooseUs = ({ image, title, benefits }) => {
+  return (
+    <section className={styles.whychooseussection}>
+      <div className={styles.whychooseuscontainer}>
+        <h3 className={styles.whychooseustitle}>{title}</h3>
+
+        <h6 className={styles.whychooseustitle1}>
+          Discover why we are your ultimate frsh food and grocery destination
+        </h6>
+
+        <div className={styles.whychooseusimagewrapper}>
+          <img
+            src={image}
+            alt="Why Choose Us"
+            className={styles.whychooseusimage}
+          />
+        </div>
+
+        <div className={styles.whychooseuscontent}>
+          <ul className={styles.whychooseusbenefits}>
+            {benefits.map((benefit, index) => (
+              <li key={index} className={styles.benefititem}>
+                <span className={styles.benefiticon}>✅</span>
+                <p>{benefit}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WhyChooseUs;
